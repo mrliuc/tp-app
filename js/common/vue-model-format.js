9d65cd3a7d0c1345d00e8050953d370688032be3
mrliuc <mrliuc@qq.com>
2017-11-07 14:58:33 +0800

    1.0.16

--- js/common/vue-model-format.js
+++ js/common/vue-model-format.js
@@ -1,6 +1,10 @@
-Vue.prototype.formatOrderType=function(orderType){
-	if(!orderType){
+Vue.prototype.formatOrderType = function(orderType) {
+	if(!orderType) {
 		return null;
 	}
 	return Model.OrderTypeName[orderType];
 }
+
+Vue.prototype.formatDateTime = function(time) {
+	return moment(time).format('YYYY-MM-DD HH:mm');
+};
