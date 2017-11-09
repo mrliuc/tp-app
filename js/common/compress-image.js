1610eaa25505f5e180eb2cd3580097bb4645aeec
mrliuc <mrliuc@qq.com>
2017-11-09 13:49:33 +0800

    1.0.18

--- js/common/compress-image.js
+++ js/common/compress-image.js
@@ -11,20 +11,22 @@
 		img.onload = function() {
 			var that = img;
 			//生成比例 
-			var w = that.width,
-				h = that.height,
+			var w = that.width/2,
+				h = that.height/2,
 				scale = w / h;
-			w = 480 || w; //480  
-			h = w / scale;
+//			w = 800 || w; //480  
+//			h = w / scale;
+
 			//生成canvas
 			var canvas = document.createElement('canvas');
 			var ctx = canvas.getContext('2d');
 
+//console.log(w+','+h);
 			canvas.width = w;
 			canvas.height = h;
 
 			ctx.drawImage(that, 0, 0, w, h);
-			var base64 = canvas.toDataURL('image/jpeg', 0.9);
+			var base64 = canvas.toDataURL('image/jpeg', 0.8);
 			var data = base64.split(",")[1];
 			callback(data,base64);
 		}
